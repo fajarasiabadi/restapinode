1ca7520e98fa18764ea75a280450d2d3bc42e3fe
fajarasiabadi <alfajr@gmail.com>
2020-10-23 00:01:59 +0700

    menampilkan semua data barang

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     respond.ok("Running Alhamdulillah", res)
 };
+
+// menampilkan data barang
+exports.getallbarang = function (req, res) {
+    connection.query(
+        'SELECT b.kode_barang, b.nama_barang, b.harga, b.stok, jb.jenis_barang, mb.merk_barang FROM barang b LEFT JOIN jenis_barang jb ON jb.id_jenis = b.id_jenis LEFT JOIN merk_barang mb ON mb.id_merk = b.id_merk WHERE mb.merk_barang IS NOT NULL', function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                respond.ok(rows, res);
+            }
+        });
+};
