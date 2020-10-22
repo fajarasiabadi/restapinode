90f2b7a4d8e8ff0075f422aa1da8f8408648a943
fajarasiabadi <alfajr@gmail.com>
2020-10-22 23:09:52 +0700

    add file controller.js

---
+++ controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var respond = require('./res');
+var connection = require('./koneksi');
+
+exports.index = function (req, res) {
+    respond.ok ("Running Alhamdulillah")
+};
--- res.js
+++ res.js
@@ -2,10 +2,10 @@
 
 exports.ok = function (values, res) {
     var data = {
-        'status' : 200,
-        'values' : values
+        'status': 200,
+        'values': values
     };
 
-     res.json(data);
-     res.end();
+    res.json(data);
+    res.end();
 }
